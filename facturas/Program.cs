5f4c3c815b0a3c07128169e415fdc948e420f3b7
daniel barron <danyy20022013@gmail.com>
2025-11-10 11:20:41 -0700

    commit bueno

---
+++ facturas/Program.cs
@@ -0,0 +1,26 @@
+
+using facturas.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSingleton<facturas.Components.Data.ServicioFacturas>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
